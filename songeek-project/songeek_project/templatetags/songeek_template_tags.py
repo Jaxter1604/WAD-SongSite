da375228ab653706356c66e93ebcce58317b2b49
Jack Little <2874766L@student.gla.ac.uk>
2025-03-11 15:49:50 +0000

    Bug fixes on album.html

--- songeek-project/songeek_project/templatetags/songeek_template_tags.py
+++ songeek-project/songeek_project/templatetags/songeek_template_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from songeek_project.models import Album, Playlist
+from songeek_project.models import Album
 
 register = template.Library()
 
@@ -8,7 +8,7 @@ def get_album_list(current_album=None):
     return {'albums': Album.objects.all(),
             'current_album': current_album}
 
-@register.inclusion_tag('songeek/profile.html')
-def get_playlists(current_album=None):
-    return {'playlists': Playlist.objects.all(),
-            'current_playlist': current_playlist}
+# @register.inclusion_tag('songeek/profile.html')
+# def get_playlists(current_album=None):
+#     return {'playlists': Playlist.objects.all(),
+#             'current_playlist': current_playlist}
