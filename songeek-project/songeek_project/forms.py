b72f43e4810d2cb66da48b491eb6220626de7c92
Bethany Oshafi Iyamu <2854627O@student.gla.ac.uk>
2025-03-21 22:14:49 +0000

    fixing create album page

--- songeek-project/songeek_project/forms.py
+++ songeek-project/songeek_project/forms.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 # no prompt for cover image
 # works via user testing
 class AlbumForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the Album name:")
-    artist = forms.CharField(max_length=128, help_text="Please enter Artist name:")
+    name = forms.CharField(max_length=128, help_text="Enter the Album name")
+    artist = forms.CharField(max_length=128, help_text="Enter Artist name")
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
@@ -19,8 +19,8 @@ class AlbumForm(forms.ModelForm):
 # minor testing needed especially with length field
 # listens will be set by admin in background or by script
 class SongForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the song name:")
-    length = forms.DurationField(help_text="Please enter the length of the song:", initial=0)
+    title = forms.CharField(max_length=128, help_text="Enter the song name")
+    length = forms.DurationField(help_text="Enter the length of the song", initial=0)
     listens = forms.IntegerField(widget=forms.HiddenInput, initial=0)
 
     class Meta:
